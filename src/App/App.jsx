104ef2de405e08454812bee737666e8ae252f9ff
Sirbu Gabriel <sirbugabriel97@gmail.com>
2023-05-11 00:23:41 +0300

    finished homepage

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,18 +1,35 @@
-import Background from "../Components/Background/Background"
-import './App.scss'
-import SolarSystem from "../Components/SolarSystem/SolarSystem"
-import Navbar from "../Components/Navbar/Navbar"
+import "./App.scss";
+import Background from "../Components/Background/Background";
+import SolarSystem from "../Components/SolarSystem/SolarSystem";
+import Navbar from "../Components/Navbar/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Earth from "../Pages/Earth/Earth";
+import Jupiter from "../Pages/Jupiter/Jupiter";
+import Mars from "../Pages/Mars/Mars";
+import Mercury from "../Pages/Mercury/Mercury";
+import Neptune from "../Pages/Neptune/Neptune";
+import Saturn from "../Pages/Saturn/Saturn";
+import Uranus from "../Pages/Uranus/Uranus";
+import Venus from "../Pages/Venus/Venus";
 
 function App() {
- 
-
   return (
-    <>
-    <Navbar/>
-    <Background/>
-    <SolarSystem/>
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Background />
+      <Routes>
+        <Route path="/" element={<SolarSystem />}></Route>
+        <Route path="/earth" element={<Earth />}></Route>
+        <Route path="/jupiter" element={<Jupiter />}></Route>
+        <Route path="/mars" element={<Mars />}></Route>
+        <Route path="/mercury" element={<Mercury />}></Route>
+        <Route path="/neptune" element={<Neptune />}></Route>
+        <Route path="/saturn" element={<Saturn />}></Route>
+        <Route path="/uranus" element={<Uranus />}></Route>
+        <Route path="/venus" element={<Venus />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,26 @@
-import "./Navbar.scss"
-import { Link } from "react-router-dom"
+import "./Navbar.scss";
+import { Link } from "react-router-dom";
+import { planets } from "../../data";
 
 const Navbar = () => {
   return (
     <header className="nav">
-        <Link>THE PLANETS</Link>
-        
+      <div className="container">
+        <h1>
+          <Link className=" title">THE PLANETS</Link>
+        </h1>
+        <div className="planets">
+          {planets.map((planet) => {
+            return (
+              <Link key={planet.id} to={planet.name} className="link">
+                {planet.name}
+              </Link>
+            );
+          })}
+        </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
